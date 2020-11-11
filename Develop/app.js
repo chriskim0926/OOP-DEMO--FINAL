bd724895c05930404399df399a42cd0b86661c79
chris kim <chriskim0926@gmail.com>
2020-11-11 12:55:26 -0500

    add rendering prompt questions to app.js

--- Develop/app.js
+++ Develop/app.js
@@ -1,36 +1,156 @@
-const Manager = require("./lib/Manager");
+const render = require('./lib/htmlRenderer');
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
+const Manager = require("./lib/Manager");
 const path = require("path");
+const inquirer = require("inquirer")
 const fs = require("fs");
-const Store = require("../Develop/test");
-
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-
-const render = require("./lib/htmlRenderer");
 
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+const outputPath = path.resolve(__dirname, "output", "main.html");
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+const teamMember = [];
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+function mainApp() {
+    // create a manager
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "What is the managers name?"
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is the managers id?"
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is the managers email?"
+            },
+            {
+                type: "input",
+                name: "officeNumber",
+                message: "What is the your phone number?"
+            }
+        ])
+        .then(answers => {
+            var { name, id, email, officeNumber } = answers;
+            var manager = new Manager(name, id, email, officeNumber);
+            
+            //Adds the managert to the team array
+            teamMember.push(manager);
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+            //Initiates teh prompt to ask for more team members
+            createTeam();
+        })
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+}
+// this function create a list to add teammembers
+function createTeam() {
+
+    inquirer
+        .prompt([
+        {
+            type: "list",
+            name: "command",
+            message:"Would you like to add more team members?",
+            choices:["Add an Engineer", "Add an Intern", "Make team"]
+        }
+        ])
+        .then(answers => {
+            // create a switch statement to choose between engineer, intern, or build team
+            statement = answers.command;
+
+            switch(statement){
+                case "Add an Engineer":
+                    getEngineer();
+                    break;
+
+                case "Add an Intern":
+                    getIntern();
+                    break;
+
+                case "Make team":
+                    buildTeam();
+                    break;
+            }
+        })
+}
+
+// a function that create an engineer
+function getEngineer() {
+
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "What is the engineers name?"
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is the engineers id?"
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is the engineers email?"
+            },
+            {
+                type: "input",
+                name: "github",
+                message: "What is the engineers github username?"
+            }
+
+        ])
+        .then(answers => {
+            var {name, id, email, github} = answers;
+            var engineer = new Engineer(name, id, email, github);
+            teamMember.push(engineer);
+        })
+
+}
+// a function that create an intern
+function getIntern() {
+
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "What is the Interns name?"
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is the Interns id?"
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is the Interns email?"
+            },
+            {
+                type: "input",
+                name: "school",
+                message: "What is the school name?"
+            }
+
+        ])
+        .then(answers => {
+            var {name, id, email, school} = answers;
+            var intern = new Intern(name, id, email, school);
+
+        })
+
+}
+
+function buildTeam() {
+    fs.writeFileSync(outputPath, render (teamMember), "utf-8");
+}
+
+mainApp()
